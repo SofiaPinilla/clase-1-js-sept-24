d815224ddda9b72b1149000841caf13d4d16e9cc
SofiaPinilla <contact@sofiapinilla.com>
2024-10-07 12:16:40 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,82 @@
+/* console.log("Hola")
+        console.warn("Esto es una advertencia")
+        console.error("Esto es un error")*/
+// alert("OJO!!")
+// console.log("EYY"); // aqui hay un console.log
+
+let nombre = "Breyner"; //string
+let edad;
+edad = 10;
+// console.log(edad);
+nombre = "Catalina";
+// console.log(nombre);
+
+const apellido = "Garcia";
+// apellido = "Pepito"
+// console.log(apellido);
+
+const name = "Sofía";
+const age = 32;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z; // undefined
+
+let number = 0;
+
+// console.log(age)
+const s = "Hello World";
+let val;
+val = s.length;
+
+val = s.toUpperCase();
+val = s.toLowerCase();
+
+val = s.substring(6, 11);
+//  console.log(val)
+
+const numbers = [1, 2, 3, 7, 5];
+const fruits = ["apples", "oranges", "pears", "grapes", "oranges"];
+const fruitsAndNumbers = [7, 2, "manzana", true, null, fruits];
+
+// console.log(fruitsAndNumbers[5][2][3])
+
+// fruits[10] = "Gabriel"
+//   fruits[4] = "blueberries";
+
+fruits.push("strawberries");
+
+fruits.unshift("mangos");
+
+fruits.pop();
+
+// console.log(fruits)
+//       console.log(fruits.indexOf("oranges"));
+
+const person = {
+  name: "Edu",
+  age: 30,
+  hobbies: ["music", "movies", "sports"],
+  address: {
+    street: "50 Main st",
+    city: "Boston",
+    state: "MA",
+  },
+};
+
+person.email= "edu@gmail.com"
+
+console.log(person.name);
+
+// const posts = [
+//   {
+//     title: "titulo1",
+//     body: "la publicacion blablabla 1",
+//   },
+//   {
+//     title: "titulo2",
+//     body: "la publicacion blablabla",
+//   },
+// ];
+
+// console.log(posts[0].body)
